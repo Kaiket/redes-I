277b6891cd6aecc93bf42646d1544caf521f2403
rual93 <rual93@gmail.com>
2013-10-05 22:03:37 +0200

    Empezada la practica, el archivo raiz no esta casi tocado, se ha creado un archivo de cabecera para aparentar estilo xD

---
+++ P2/practica2.h
@@ -0,0 +1,28 @@
+/***************************************************************************
+ * Autores: Enrique Cabrerizo Fernández, Guillermo Ruiz Álvarez
+ * Archivo: practica1.c
+ * Compilar: gcc -o practica1 practica1.c -lpcap
+ ***************************************************************************/
+
+#ifndef __PRACTICA2_H
+#define __PRACTICA2_H
+
+/*
+ * Analiza un paquete imprimiendo en el standard output la informacion
+ * del mismo.
+ * Recibe: Puntero al inicio del paquete, cabecera del paquete, contador de paquetes.
+ * Devuelve: 0 si no han habido errores.
+ *	     1 si se le pasan argumentos invalidos (e.g punteros a NULL).
+ */
+u_int8_t analizarPaquete(u_int8_t*,struct pcap_pkthdr*,u_int64_t);
+
+
+
+/*
+ * Funcion manejadora de la señal Ctrl+C (SIGINT).
+ * Recibe: el numero de la señal que se ha enviado, en este caso SIGINT.
+ */
+void handleSignal(int nsignal);
+
+
+#endif /*PRACTICA2__H*/
