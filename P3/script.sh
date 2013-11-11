3177f50167bcb95d749dd8e592821470c1d680c5
rual93 <rual93@gmail.com>
2013-11-11 12:02:33 +0100

    Script

--- P3/script.sh
+++ P3/script.sh
@@ -1,6 +1,33 @@
 #!bin/bash
-echo "Popularidad de puertos por paquetes"
-cut -f 5 datosPORTS | sort -k 1 | uniq -c | sort -nk 1 -r | head -n 5 #popularidad de puertos por paquetes
-echo ""
-echo "Popularidad de IP por paquetes"
-cut -f 4 datosIP | sort -k 1 | uniq -c | sort -nk 1 -r | head -n 5 #popularidad de IP por paquetes
+
+	echo ""
+
+#Popularidad de IP
+	#Por Paquetes:
+	echo "Popularidad de IPs por paquetes (TOP 5):"
+	echo "IP\t\t N.Paquetes."
+	cut -f 4 datosIP | sort -k 1 | uniq -c | sort -nk 1 -r | head -n 5 | awk '{print $2, "\t", $1}'
+
+	echo ""
+	
+	#Por Bytes:
+	echo "Popularidad de IPs por bytes (TOP 5):"
+	echo "IP\t\t Bytes"
+	cut -f 3,4 datosIP | awk '{data[$2]+=$1} END {for (elem in data) {print elem, "\t", data[elem]}}' | sort -r -k 2 -n | head -n 5
+
+	echo ""
+
+#Popularidad de Puertos:
+	#Por Paquetes:
+	echo "Popularidad de puertos por paquetes (TOP 5):"
+	echo "Puertos\t N.Paquetes"
+	cut -f 5 datosPORTS | sort -k 1 | uniq -c | sort -nk 1 -r | head -n 5 | awk '{print $2, "\t", $1}'
+
+	echo ""
+
+	#Por Bytes:
+	echo "Popularidad de puertos por bytes (TOP 5):"
+	echo "Puertos\t Bytes"
+	cut -f 3,5 datosPORTS | awk '{data[$2]+=$1} END {for (elem in data) {print elem, "\t", data[elem]}}' | sort -r -k 2 -n | head -n 5
+
+	echo ""
